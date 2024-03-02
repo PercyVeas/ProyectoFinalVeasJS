bb37a28052d063a6f2000721bed5d3eb5c86b123
PercyVeas <percy.veas.pizarro@gmail.com>
2024-03-02 01:11:02 -0400

    maquetando

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// Clave de API de TMDB (reemplaza 'TU_API_KEY' con tu propia clave)
+// Clave de API de TMDB
 const apiKey = 'b20102a953a25b6f42cd5e5660dc717f';
 
 // URL base de la API de TMDB
@@ -17,31 +17,31 @@ const moviesContainer = document.getElementById('movies');
 // Realizar la solicitud a la API de TMDB
 fetch(url)
     .then(response => {
-    // Verificar si la respuesta es exitosa
-    if (!response.ok) {
-        throw new Error('La solicitud no fue exitosa');
-    }
-    // Convertir la respuesta a formato JSON
-    return response.json();
-})
+        // Verificar si la respuesta es exitosa
+        if (!response.ok) {
+            throw new Error('La solicitud no fue exitosa');
+        }
+        // Convertir la respuesta a formato JSON
+        return response.json();
+    })
     .then(data => {
-    // Manejar los datos obtenidos de la API
-    const movies = data.results;
-    // Iterar sobre las películas y mostrar las títulos y pósters en la página
-    movies.forEach(movie => {
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('card'); // Agregar clase 'card' a la tarjeta
-        movieElement.innerHTML = `
-        <h2>${movie.title}</h2>
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" height:300px; width:300px;>`;
-        moviesContainer.appendChild(movieElement);
-    });
-})
+        // Manejar los datos obtenidos de la API
+        const movies = data.results;
+        // Iterar sobre las películas y mostrar los títulos y pósters en la página
+        movies.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('card'); // Agregar clase 'card' a la tarjeta
+            movieElement.innerHTML = `
+                <h2>${movie.title}</h2>
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" height="300" width="300">
+            `;
+            moviesContainer.appendChild(movieElement);
+        });
+    })
     .catch(error => {
-    // Manejar cualquier error que ocurra durante la solicitud
-    console.error('Error al obtener películas populares:', error);
-});
-
+        // Manejar cualquier error que ocurra durante la solicitud
+        console.error('Error al obtener películas populares:', error);
+    });
 
 
 
